ecc9bfa9f17ebf84d3fd31bad292e1d7813ac492
Chris Friemann <github@chris.friemann.nl>
2023-11-01 12:52:29 +0100

    :sparkles: added ToDoController

---
+++ src/main/java/nl/chris/ToDoController.java
@@ -0,0 +1,83 @@
+package nl.chris;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class ToDoController {
+    public ToDoManager manager = new ToDoManager();
+    private ToDoListView list = new ToDoListView(this);
+    private ToDoDetailView detailView = new ToDoDetailView(this);
+
+    /**
+     * Constructor
+     */
+    public ToDoController() {
+        // Create instances
+
+        // Create temporary mock data
+        addToDoItem("Test", false);
+        addToDoItem("Test2", true);
+        addToDoItem("Test3", false);
+
+        // Show all ToDoItems
+        ShowList(manager.getToDoItems());
+
+    }
+
+    /**
+     * Show all ToDoItems
+     * @param toDoItems - The ToDoItem array
+     */
+    public void ShowList(ArrayList<ToDoItem> toDoItems) {
+        list.ShowList(toDoItems);
+    }
+
+    /**
+     * Show a specific ToDoItem
+     * @param toDoItem - The ToDoItem object
+     */
+    public void ShowToDoItem(ToDoItem toDoItem) {
+        detailView.ShowDetail(toDoItem);
+    }
+
+    /**
+     * Add a ToDoItem
+     * @param name - The name of the ToDoItem
+     * @param isDone - The status of the ToDoItem
+     * @return - The ToDoItem object
+     */
+    public ToDoItem addToDoItem(String name, boolean isDone) {
+        ToDoItem item = manager.addToDoItem(name, isDone);
+        return item;
+    }
+    public ToDoItem addToDoItem(String name, boolean isDone,boolean updateListview) {
+        ToDoItem item = this.addToDoItem(name,isDone);
+        if(updateListview)
+             list.repaintList(manager.getToDoItems());
+        return item;
+    }
+    /**
+     * Edit a ToDoItem
+     * @param toDoItem - The ToDoItem object to edit
+     * @param name - The new name of the ToDoItem
+     * @param isDone - The new status of the ToDoItem
+     */
+    public ToDoItem editToDoItem(ToDoItem toDoItem, String name, boolean isDone) {
+        ToDoItem item = manager.editToDoItem(toDoItem, name, isDone);
+        return item;
+    }
+    public ToDoItem editToDoItem(ToDoItem toDoItem, String name, boolean isDone,boolean updateListview) {
+        this.editToDoItem(toDoItem,name,isDone);
+        if(updateListview)
+            list.repaintList(manager.getToDoItems());
+        return toDoItem;
+    }
+
+    /**
+     * Remove a ToDoItem
+     * @param toDoItem - The ToDoItem object to remove
+     */
+    public void removeToDoItem(ToDoItem toDoItem) {
+        manager.removeToDoItem(toDoItem);
+    }
+}
